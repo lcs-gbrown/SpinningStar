f50a16b7b7aa6e34cc4f7f2924561ddc733a68b5
gabi brown <gbrown@lcs.on.ca>
2022-04-27 09:56:35 -0400

    Animation slides star from left to right. All three requirements met (initial state, trigger, logic to change the state).

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties
+   
+    //1. Starting position for the star (initial state)
+    @State var xOffset = -100.0
+    
+    //MARK: Computed Properties
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "star")
+            .resizable()
+            .frame(width: 40, height: 40)
+            .foregroundColor(.yellow)
+            .animation(.default)
+            .offset(x: xOffset, y: 0)
+        // 3. Trigger the animation on a tap
+            .onTapGesture {
+                // 2. Logic that changes the state
+                xOffset = 100
+            }
     }
 }
 
